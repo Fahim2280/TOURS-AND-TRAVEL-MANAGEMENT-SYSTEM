abdbf9190c4d3319e92833df0f79842654591b25
Md. Fahim Alam <76841119+Fahim2280@users.noreply.github.com>
2022-07-31 00:14:19 +0600

    change

--- controller/evenDataUplode.php
+++ controller/evenDataUplode.php
@@ -7,13 +7,6 @@ $H_M_day=$_POST['H_M_day'];
 $Event=$_POST['Event'];
 $image=$_POST['image'];
 
-$sql = "INSERT INTO user (uplod,Name,H_M_day,Event,image) VALUES(?,?,?,?)";
-$stmtimnsert = $db->prepare($sql);
-$result = $stmtimnsert->execute([$Name , $H_M_day , $Event , $image ])
-if($result){
-    echo 'Successfully save';
-}else{
-    echo '';
-}
+
 
 }

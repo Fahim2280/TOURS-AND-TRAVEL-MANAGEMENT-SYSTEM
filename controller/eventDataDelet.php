d49898a6870d1f5dcc2860ef8add69c53ef641c8
Md. Fahim Alam <76841119+Fahim2280@users.noreply.github.com>
2022-08-06 15:25:57 +0600

    delete add

---
+++ controller/eventDataDelet.php
@@ -0,0 +1,14 @@
+<?php
+require_once ('../model/dbConnection.php');
+
+$SERIAL = $_REQUEST['SERIAL'];
+
+if ($SERIAL == null) {
+    echo "<h1 align= center> PLEASE ADD INFORMATION <br><div align=center><a href=train.php> BACK </a></div>";
+    return;
+} else {
+    $data['serial'] = $SERIAL;
+deleteEvent($data);
+}
+//header("location: ../view/eventData.html");
+?>

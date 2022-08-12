41df6b0001f3868a911f284b926468808a903a0b
Md. Fahim Alam <76841119+Fahim2280@users.noreply.github.com>
2022-08-13 00:45:02 +0600

    slide show add

--- index.php
+++ index.php
@@ -7,8 +7,37 @@
 <body>
 
     <link rel="stylesheet" href="./view/eventCard.css" />
+    <link rel="stylesheet" href="./view//slideShow.css" />
 
     <form action="" method="POST" enctype="">
+        <div class="slideshow-container">
+
+            <div class="mySlides fade">
+                <div class="numbertext">1 / 3</div>
+                <img src="./image/Cox.jpg" style="width:100%">
+                <div class="text">Caption Text</div>
+            </div>
+
+            <div class="mySlides fade">
+                <div class="numbertext">2 / 3</div>
+                <img src="./image/sentmartinjpg.jpg" style="width:100%">
+                <div class="text">Caption Two</div>
+            </div>
+
+            <div class="mySlides fade">
+                <div class="numbertext">3 / 3</div>
+                <img src="./image/The House Boat.jpg" style="width:100%">
+                <div class="text">Caption Three</div>
+            </div>
+
+        </div>
+        <br>
+
+        <div style="text-align:center">
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+        </div>
         <fieldset style="background-color:DodgerBlue;">
             <h1>Booking.com</h1>
             <table>
@@ -50,6 +79,7 @@
             }
             ?>
         </div>
+        <script src="./controller/slideShow.js"></script>
 </body>
 
 

43bd1dfe652de1efb311e7ee4a941fd476664c9a
Md. Fahim Alam <76841119+Fahim2280@users.noreply.github.com>
2022-08-22 02:44:52 +0600

    up

--- controller/validation.js
+++ controller/validation.js
@@ -1,25 +1,4 @@
 function eventvalidated() {
-  let Name = document.getElementById("Name").value;
-  let H_M_day = document.getElementById("H_M_day").value;
-  let Description = document.getElementById("Description").value;
-  let Place_Name = document.getElementById("Place_Name").value;
-  let Food = document.getElementById("Food").value;
-  let Price = document.getElementById("Price").value;
-  let file = document.getElementById("file").value;
-  let SERIAL = document.getElementById("SERIAL").value;
-  if (
-    Name != "" ||
-    H_M_day != "" ||
-    Description != "" ||
-    Place_Name != "" ||
-    Food != "" ||
-    Price != "" ||
-    file != "" ||
-    SERIAL != ""
-  ) {
-    return true;
-  } else {
-    alert(" cannot be blank");
-    return false;
-  }
+ 
+  
 }

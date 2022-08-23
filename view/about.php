195572d08e61864916dc72393e5b849231050869
Md. Fahim Alam <76841119+Fahim2280@users.noreply.github.com>
2022-08-23 23:39:50 +0600

    Update about.php

--- view/about.php
+++ view/about.php
@@ -1,6 +1,19 @@
 <!DOCTYPE html>
-<body>
-    <p>Md Fahim Alam 20-42517-1 <br></p>
-</body>
-</html>
 
+<body>
+    <p>Creat account as travel admin
+
+        *if your create accout you can add
+        your transport rout your transport shedule
+        then we use your data and show it to customer
+        after that we purchase ticket an send customar
+
+        <br>
+        <br>
+    </p>
+    <p>
+        $30 for 1st subscription <br>* you can add your transport<br> * you can add trips ,update trips<br> * $20 per month. <br>
+    </p>
+</body>
+
+</html>
